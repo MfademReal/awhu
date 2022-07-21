71b89d696d2186e3f87e97ea63e3e4b95c32c78e
awht-team <109755149+awht-team@users.noreply.github.com>
2022-07-21 17:42:49 +0000

    Delete test.py

--- awhu/test.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from telethon.sync import TelegramClient
-apiHash = "7fd33cdfb5bf201e33e98ea27b682389"
-app_id = "17687296"
-botToken = "5225075398:AAHlPZY7cJGKDBDLxGgfcqk5M9gixTK9KFk"
-
-bot = TelegramClient('bot',app_id, apiHash).start(bot_token=botToken)
-bot.send_file("@iMr_Fun","/home/mrfun/Downloads/texture/test.torrent")
